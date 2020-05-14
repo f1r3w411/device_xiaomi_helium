6cff68a1ae6e1468da8d292990c9874e5d05baa8
f1r3w411 <novikov.firewall@gmail.com>
2020-05-14 10:26:20 +0300

    Rebellion changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,10 +16,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_helium.mk \
+    $(LOCAL_DIR)/rebellion_helium.mk \
     $(LOCAL_DIR)/full_helium.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_helium-user \
-    xtended_helium-userdebug \
-    xtended_helium-eng
+    rebellion_helium-user \
+    rebellion_helium-userdebug \
+    rebellion_helium-eng
